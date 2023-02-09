b67f4980fae476f7a4a90d3915de2ab4edda9de2
Lucas <lucaspedrosadesouza@gmail.com>
2023-02-09 19:18:22 -0300

    initProject

---
+++ PokemonAPI/src/components/HeaderComponents.vue
@@ -0,0 +1,8 @@
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+@media (min-width: 768px) {
+}
+</style>
---
+++ PokemonAPI/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// import ButtonComponents from "../components/ButtonComponents.vue";
+// import CardComponents from "../components/CardComponents.vue";
+import HeaderComponents from "../components/HeaderComponents.vue";
+// import ModalComponents from "../components/ModalComponents.vue";
+</script>
+
+<template>
+  <HeaderComponents />
+  <main>
+    <TheWelcome />
+  </main>
+</template>
