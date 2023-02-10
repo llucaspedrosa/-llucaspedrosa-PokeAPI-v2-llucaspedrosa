b499c45ce7461d02e531d74fe72cdbf3de68c290
Lucas <lucaspedrosadesouza@gmail.com>
2023-02-10 01:07:36 -0300

    headerDone

--- PokemonAPI/src/views/HomePage.vue
+++ PokemonAPI/src/views/HomePage.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 // import ButtonComponents from "../components/ButtonComponents.vue";
-// import CardComponents from "../components/CardComponents.vue";
+import CardComponents from "../components/CardComponents.vue";
 import HeaderComponents from "../components/HeaderComponents.vue";
-// import ModalComponents from "../components/ModalComponents.vue";
+import InputComponents from "../components/InputComponents.vue";
 </script>
 
 <template>
   <HeaderComponents />
-  <main>
-    <TheWelcome />
-  </main>
 </template>
+<style scoped></style>
